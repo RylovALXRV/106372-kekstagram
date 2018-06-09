79eadd48ec99b2d3be12922cf67a63e0bd420779
Алексей <rylovalex2108l@gmail.com>
2018-06-09 19:41:28 +0700

    внес исправления по правильным переборам массива, все вызовы функций перенес вниз

--- js/pictures.js
+++ js/pictures.js
@@ -20,69 +20,78 @@ var desc = [
   'Вот это тачка!'
 ];
 
-var pictures = [];
-
 var getRandomFeature = function (features) {
   return features[Math.floor(Math.random() * (features.length - 1))];
 };
 
+
+// Math.random() же возвращает [0; 1), т.е. от 0 до но не включая 1 (0,999999), а если я
+// прибавлю 0.5 и если округлю значение в меньшую сторону, то войдет в промежуток и 0 и 1
 var getRandomInteger = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min + 0.5);
+};
+
+// а по комментарию мыслей нет больше на этот момент
+var getRandomComments = function () {
+  var arr = [];
+  for (var i = 0; i < comments.length; i++) {
+    if (getRandomInteger(1, 2) === 1) {
+      arr.push(getRandomFeature(comments));
+    } else {
+      arr.push(getRandomFeature(comments) + ' ' + getRandomFeature(comments));
+    }
+  }
+  return arr;
 };
 
 var createPicture = function (num) {
   return {
     url: 'photos/' + (num + 1) + '.jpg',
     likes: getRandomInteger(1, 25),
-    comments: function () {
-      return (getRandomInteger(1, 2) === 1) ?
-        getRandomFeature(comments) :
-        getRandomFeature(comments) + ' ' + getRandomFeature(comments);
-    },
+    comments: getRandomComments(),
     description: getRandomFeature(desc)
   };
 };
 
-for (var i = 0; i < 25; i++) {
-  pictures.push(createPicture(i));
-}
+var pictures = Array.apply(null, {length: 25}).map(Function.call, createPicture);
 
 var renderPicture = function (picture) {
   var pictureElement = templateAnchor.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = picture.url;
   pictureElement.querySelector('.picture__stat--likes').textContent = picture.likes;
-  pictureElement.querySelector('.picture__stat--comments').textContent = Math.floor(Math.random() * comments.length);
+  pictureElement.querySelector('.picture__stat--comments').textContent = picture.comments.length;
   return pictureElement;
 };
 
 var renderPictures = function (allPictures) {
   var fragment = document.createDocumentFragment();
-  for (var j = 0; j < allPictures.length; j++) {
-    fragment.appendChild(renderPicture(allPictures[j]));
-  }
+  allPictures.forEach(function (item) {
+    fragment.appendChild(renderPicture(item));
+  });
   document.querySelector('.pictures').appendChild(fragment);
+};
+
+var renderBigPicture = function (bigPicture) {
+  var socialComments = document.querySelectorAll('.social__comment');
+  document.querySelector('.big-picture__img img').src = bigPicture.url;
+  document.querySelector('.likes-count').textContent = bigPicture.likes;
+  document.querySelector('.comments-count').textContent = bigPicture.comments.length;
+  socialComments.forEach(function (item) {
+    item.querySelector('.social__picture').src = 'img/avatar-' + getRandomInteger(1, 6) + '.svg';
+    item.querySelector('.social__text').textContent = getRandomFeature(bigPicture.comments);
+  });
+  document.querySelector('.social__caption').textContent = bigPicture.description;
+};
+
+var hideString = function (parentElem, text) {
+  parentElem.classList.add(text);
 };
 
 renderPictures(pictures);
 
 document.querySelector('.big-picture').classList.remove('hidden');
 
-var renderBigPicture = function (bigPicture) {
-  var socialComments = document.querySelectorAll('.social__comment');
-  document.querySelector('.big-picture__img img').src = bigPicture.url;
-  document.querySelector('.likes-count').textContent = bigPicture.likes;
-  document.querySelector('.comments-count').textContent = getRandomInteger(1, 2);
-  for (var k = 0; k < socialComments.length; k++) {
-    socialComments[k].querySelector('.social__picture').src = 'img/avatar-' + getRandomInteger(1, 6) + '.svg';
-    socialComments[k].querySelector('.social__text').textContent = bigPicture.comments();
-  }
-  document.querySelector('.social__caption').textContent = bigPicture.description;
-};
 renderBigPicture(pictures[0]);
 
-var hideOrShowString = function (parentElem, text) {
-  parentElem.classList.toggle(text);
-};
-
-hideOrShowString(document.querySelector('.social__comment-count'), 'visually-hidden');
-hideOrShowString(document.querySelector('.social__loadmore'), 'visually-hidden');
+hideString(document.querySelector('.social__comment-count'), 'visually-hidden');
+hideString(document.querySelector('.social__loadmore'), 'visually-hidden');
