4fc43dd58ce009fab9c6cd0ff22d0118ebcbb181
Алексей <rylovalex2108l@gmail.com>
2018-06-12 10:39:32 +0700

    переименовал некоторые переменные и функции, исправил логику функций, перенес массив pictures вниз файла

--- js/pictures.js
+++ js/pictures.js
@@ -2,7 +2,7 @@
 
 var templateAnchor = document.querySelector('#picture').content.querySelector('.picture__link');
 
-var comments = [
+var exampleComments = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -11,7 +11,7 @@ var comments = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-var desc = [
+var exampleDescriptions = [
   'Тестим новую камеру!',
   'Затусили с друзьями на море',
   'Как же круто тут кормят',
@@ -20,40 +20,36 @@ var desc = [
   'Вот это тачка!'
 ];
 
-var getRandomFeature = function (features) {
-  return features[Math.floor(Math.random() * (features.length - 1))];
+var getRandomElement = function (element) {
+  return element[Math.floor(Math.random() * (element.length - 1))];
 };
 
-
-// Math.random() же возвращает [0; 1), т.е. от 0 до но не включая 1 (0,999999), а если я
-// прибавлю 0.5 и если округлю значение в меньшую сторону, то войдет в промежуток и 0 и 1
 var getRandomInteger = function (min, max) {
-  return Math.floor(Math.random() * (max - min) + min + 0.5);
+  return Math.round(Math.random() * (max - min) + min);
 };
 
-// а по комментарию мыслей нет больше на этот момент
-var getRandomComments = function () {
-  var arr = [];
-  for (var i = 0; i < comments.length; i++) {
-    if (getRandomInteger(1, 2) === 1) {
-      arr.push(getRandomFeature(comments));
-    } else {
-      arr.push(getRandomFeature(comments) + ' ' + getRandomFeature(comments));
-    }
+var createRandomComment = function () {
+  return (Math.round(Math.random()) === 0) ?
+    getRandomElement(exampleComments) :
+    getRandomElement(exampleComments) + ' ' + getRandomElement(exampleComments);
+};
+
+var generateComments = function () {
+  var comments = [];
+  for (var i = 0; i < exampleComments.length; i++) {
+    comments.push(createRandomComment());
   }
-  return arr;
+  return comments;
 };
 
 var createPicture = function (num) {
   return {
     url: 'photos/' + (num + 1) + '.jpg',
     likes: getRandomInteger(1, 25),
-    comments: getRandomComments(),
-    description: getRandomFeature(desc)
+    comments: generateComments(),
+    description: getRandomElement(exampleDescriptions)
   };
 };
-
-var pictures = Array.apply(null, {length: 25}).map(Function.call, createPicture);
 
 var renderPicture = function (picture) {
   var pictureElement = templateAnchor.cloneNode(true);
@@ -78,14 +74,12 @@ var renderBigPicture = function (bigPicture) {
   document.querySelector('.comments-count').textContent = bigPicture.comments.length;
   socialComments.forEach(function (item) {
     item.querySelector('.social__picture').src = 'img/avatar-' + getRandomInteger(1, 6) + '.svg';
-    item.querySelector('.social__text').textContent = getRandomFeature(bigPicture.comments);
+    item.querySelector('.social__text').textContent = getRandomElement(bigPicture.comments);
   });
   document.querySelector('.social__caption').textContent = bigPicture.description;
 };
 
-var hideString = function (parentElem, text) {
-  parentElem.classList.add(text);
-};
+var pictures = Array.apply(null, {length: 25}).map(Function.call, createPicture);
 
 renderPictures(pictures);
 
@@ -93,5 +87,5 @@ document.querySelector('.big-picture').classList.remove('hidden');
 
 renderBigPicture(pictures[0]);
 
-hideString(document.querySelector('.social__comment-count'), 'visually-hidden');
-hideString(document.querySelector('.social__loadmore'), 'visually-hidden');
+document.querySelector('.social__comment-count').classList.add('visually-hidden');
+document.querySelector('.social__loadmore').classList.add('visually-hidden');
